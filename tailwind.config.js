a62c7dbb467bbfb2f196d310c20cb62ee5a67336
mezealot24 <wiriya.on@gmail.com>
2024-07-14 17:53:47 +0700

    updateV.2.0.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,9 @@
 module.exports = {
 	darkMode: ["class"],
 	content: [
-		"./pages/**/*.{js,jsx}",
-		"./components/**/*.{js,jsx}",
-		"./app/**/*.{js,jsx}",
-		"./src/**/*.{js,jsx}",
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+		"./app/**/*.{js,ts,jsx,tsx}",
 	],
 	prefix: "",
 	theme: {
